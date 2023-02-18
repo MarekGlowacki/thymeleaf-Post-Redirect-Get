bc51697e303ce086b6411acff57059cb2394b42f
User <user@its.me>
2023-02-18 11:06:28 +0100

    Initial commit

---
+++ src/main/java/online/javafun/prg/ThymeleafPostRedirectGetApplication.java
@@ -0,0 +1,13 @@
+package online.javafun.prg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafPostRedirectGetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafPostRedirectGetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/online/javafun/prg/ThymeleafPostRedirectGetApplicationTests.java
@@ -0,0 +1,13 @@
+package online.javafun.prg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafPostRedirectGetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
